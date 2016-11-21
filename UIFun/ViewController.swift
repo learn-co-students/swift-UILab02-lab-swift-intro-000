4471487d70efbf45b405cb1846c8e116ef8f85b0
Rei Kacanja <reikacanja@Reis-MacBook-Pro.local>
2016-11-21 11:21:53 -0500

    Painting By Numbers Lab Complete

--- UIFun/ViewController.swift
+++ UIFun/ViewController.swift
@@ -52,22 +52,31 @@ class ViewController: UIViewController {
         switch (firstSelection, secondSelection) {
         case (0,0):
             paintBucket.backgroundColor = mixColors(first: "Red", second: "Red")
+            print("Red and red make red!")
         case (1,1):
             paintBucket.backgroundColor = mixColors(first: "Yellow", second: "Yellow")
+            print("Yellow and yellow make yellow!")
         case (2,2):
             paintBucket.backgroundColor = mixColors(first: "Blue", second: "Blue")
+            print("Blue and blue make blue!")
         case (0,1):
             paintBucket.backgroundColor = mixColors(first: "Red", second: "Yellow")
+            print("Red and yellow make orange!")
         case (1,0):
             paintBucket.backgroundColor = mixColors(first: "Yellow", second: "Red")
+            print("Yellow and red make orange!")
         case (0,2):
             paintBucket.backgroundColor = mixColors(first: "Red", second: "Blue")
+            print("Red and blue make purple!")
         case (2,0):
             paintBucket.backgroundColor = mixColors(first: "Blue", second: "Red")
+            print("Blue and red make purple!")
         case (1,2):
             paintBucket.backgroundColor = mixColors(first: "Yellow", second: "Blue")
+            print("Yellow and blue make green!")
         case (2,1):
             paintBucket.backgroundColor = mixColors(first: "Blue", second: "Yellow")
+            print("Blue and yellow make green!")
         default:
             print("Not available...")
         }
