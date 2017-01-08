8ae7edba839807fbb4d1b44fe21592b0c468f48f
edmund-h <447ght@gmail.com>
2017-01-08 16:23:45 -0500

    anything to avoid long conditional chains

--- UIFun/ViewController.swift
+++ UIFun/ViewController.swift
@@ -12,36 +12,49 @@ class ViewController: UIViewController {
 
     // TODO: Setup the IBOutlets
     
+    @IBOutlet weak var colorPickYourNose: UISegmentedControl!
+    @IBOutlet weak var colorPickYourFriendsNose: UISegmentedControl!
+    @IBOutlet weak var mrBucket: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-
-    func mixColors(first: String, second: String) -> UIColor {
         
-        switch (first, second) {
-        case ("Red", "Red"):
-            return UIColor.red
-        case ("Red", "Yellow"), ("Yellow", "Red"):
-            return UIColor.orange
-        case ("Red", "Blue"), ("Blue", "Red"):
-            return UIColor.purple
-        case ("Yellow", "Yellow"):
-            return UIColor.yellow
-        case ("Yellow", "Blue"), ("Blue", "Yellow"):
-            return UIColor.green
-        case ("Blue", "Blue"):
-            return UIColor.blue
-        default:
-            return UIColor.white
-        }
-        
+        colorPickYourNose.setTitle("Red", forSegmentAt: 0)
+        colorPickYourNose.setTitle("Yellow", forSegmentAt: 1)
+        colorPickYourNose.setTitle("Blue", forSegmentAt: 2)
+        colorPickYourFriendsNose.setTitle("Red", forSegmentAt: 0)
+        colorPickYourFriendsNose.setTitle("Yellow", forSegmentAt: 1)
+        colorPickYourFriendsNose.setTitle("Blue", forSegmentAt: 2)
     }
     
-    @IBAction func colorSelected(_ sender: UISegmentedControl) {
+/*
+    func mixColors(_ sender1: UISegmentedControl,_ sender2: UISegmentedControl) -> UIColor {
         
-        // TODO: Implement this function according to the instructions.
         
-        
+        }
     }
-
+  */
+    
+    @IBAction func disregardBeatHappening () {
+        
+        let first = colorPickYourNose.titleForSegment(at: colorPickYourNose.selectedSegmentIndex)
+        let second = colorPickYourFriendsNose.titleForSegment(at: colorPickYourFriendsNose.selectedSegmentIndex)
+        
+        switch (first, second) {
+        case ("Red"?, "Red"?) , (nil, "Red"?), ("Red"?, nil):
+            mrBucket.backgroundColor = UIColor.red
+        case ("Red"?, "Yellow"?), ("Yellow"?, "Red"?):
+            mrBucket.backgroundColor =  UIColor.orange
+        case ("Red"?, "Blue"?), ("Blue"?, "Red"?):
+            mrBucket.backgroundColor =  UIColor.purple
+        case ("Yellow"?, "Yellow"?), (nil, "Yellow"?) , ("Yellow"?, nil):
+            mrBucket.backgroundColor =  UIColor.yellow
+        case ("Yellow"?, "Blue"?), ("Blue"?, "Yellow"?):
+            mrBucket.backgroundColor =  UIColor.green
+        case ("Blue"?, "Blue"?), (nil , "Blue"?), ("Blue"?, nil):
+            mrBucket.backgroundColor =  UIColor.blue
+        default:
+            mrBucket.backgroundColor =  UIColor.white
+              }
+    }
 }
