1381bf8dd875cfe93a1263995a4010fa088b0b66
4Ring <ukrainianslavic@gmail.com>
2017-01-10 23:34:32 -0800

    added white to color list

--- UIFun/ViewController.swift
+++ UIFun/ViewController.swift
@@ -17,9 +17,9 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        firstColorSegmentedControl.selectedSegmentIndex = 0
-        secondColorSegmentedControl.selectedSegmentIndex = 0
-        paintBucket.backgroundColor = mixColors(first: "Red", second: "Red")
+        firstColorSegmentedControl.selectedSegmentIndex = 3
+        secondColorSegmentedControl.selectedSegmentIndex = 3
+        paintBucket.backgroundColor = mixColors(first: "White", second: "White")
     }
 
     func mixColors(first: String, second: String) -> UIColor {
@@ -37,6 +37,14 @@ class ViewController: UIViewController {
             return UIColor.green
         case ("Blue", "Blue"):
             return UIColor.blue
+        case ("White", "White"):
+            return UIColor.white
+        case ("White", "Red"), ("Red", "White"):
+            return UIColor(red:1.00, green:0.50, blue:0.50, alpha:1.0)
+        case ("White", "Yellow"), ("Yellow", "White"):
+            return UIColor(red:1.00, green:0.96, blue:0.50, alpha:1.0)
+        case ("White", "Blue"), ("Blue", "White"):
+            return UIColor(red:0.50, green:0.64, blue:0.84, alpha:1.0)
         default:
             return UIColor.white
         }
@@ -55,18 +63,32 @@ class ViewController: UIViewController {
             paintBucket.backgroundColor = mixColors(first: "Red", second: "Yellow")
         case (0,2):
             paintBucket.backgroundColor = mixColors(first: "Red", second: "Blue")
+        case (0,3):
+            paintBucket.backgroundColor = mixColors(first: "Red", second: "White")
         case (1,0):
             paintBucket.backgroundColor = mixColors(first: "Yellow", second: "Red")
         case (1,1):
             paintBucket.backgroundColor = mixColors(first: "Yellow", second: "Yellow")
         case (1,2):
             paintBucket.backgroundColor = mixColors(first: "Yellow", second: "Blue")
+        case (1,3):
+            paintBucket.backgroundColor = mixColors(first: "Yellow", second: "White")
         case (2,0):
             paintBucket.backgroundColor = mixColors(first: "Blue", second: "Red")
         case (2,1):
             paintBucket.backgroundColor = mixColors(first: "Blue", second: "Yellow")
         case (2,2):
             paintBucket.backgroundColor = mixColors(first: "Blue", second: "Blue")
+        case (2,3):
+            paintBucket.backgroundColor = mixColors(first: "Blue", second: "White")
+        case (3,0):
+            paintBucket.backgroundColor = mixColors(first: "White", second: "Red")
+        case (3,1):
+            paintBucket.backgroundColor = mixColors(first: "White", second: "Yellow")
+        case (3,2):
+            paintBucket.backgroundColor = mixColors(first: "White", second: "Blue")
+        case (3,3):
+            paintBucket.backgroundColor = mixColors(first: "White", second: "White")
         default:
             print ("That is not a valid choice.")
         }
