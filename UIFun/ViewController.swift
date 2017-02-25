5f6e18d930ccc5ec7f5d0ef2a0f16250f797af6a
g3kenny <g3_kenny@hotmail.com>
2017-02-25 16:55:23 +0800

    Commit UI lab2

--- UIFun/ViewController.swift
+++ UIFun/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
 
     // TODO: Setup the IBOutlets
     
+    @IBOutlet var paintBucket: UIImageView!
+    @IBOutlet var firstColorSegmentedControl: UISegmentedControl!
+    @IBOutlet var secondColorSegmentedControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -40,8 +44,26 @@ class ViewController: UIViewController {
     @IBAction func colorSelected(_ sender: UISegmentedControl) {
         
         // TODO: Implement this function according to the instructions.
+        let firstSelected = firstColorSegmentedControl.selectedSegmentIndex
+        let secondSelected = secondColorSegmentedControl.selectedSegmentIndex
+        let firstString = setString(selected: firstSelected)
+        let secondString = setString(selected: secondSelected)
+        let color = mixColors(first: firstString, second: secondString)
+        paintBucket.backgroundColor = color
+    }
+    
+    func setString(selected: Int) -> String {
+        var output = ""
+        switch selected {
+        case 0 :
+            output = "Red"
+        case 1 :
+            output = "Yellow"
+        default :
+            output = "Blue"
+        }
         
-        
+        return output
     }
 
 }
