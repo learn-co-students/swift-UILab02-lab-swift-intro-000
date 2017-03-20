992e36843409e568e39a1fcf52a526efc2bcd416
RaymondAtivie <raymondativie@gmail.com>
2017-03-20 15:45:01 +0100

    Answer

--- UIFun/ViewController.swift
+++ UIFun/ViewController.swift
@@ -11,6 +11,10 @@ import UIKit
 class ViewController: UIViewController {
 
     // TODO: Setup the IBOutlets
+    @IBOutlet weak var paintBucket: UIImageView!
+    
+    @IBOutlet weak var firstColorSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var secondColorSegmentedControl: UISegmentedControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,7 +43,26 @@ class ViewController: UIViewController {
     
     @IBAction func colorSelected(_ sender: UISegmentedControl) {
         
+        let firstSelection = firstColorSegmentedControl.selectedSegmentIndex
+        let secondSelection = secondColorSegmentedControl.selectedSegmentIndex
+        
         // TODO: Implement this function according to the instructions.
+        switch (firstSelection, secondSelection) {
+        case (0, 0):
+            paintBucket.backgroundColor = mixColors(first: "Red", second: "Red")
+        case (1, 1):
+            paintBucket.backgroundColor = mixColors(first: "Yellow", second: "Yellow")
+        case (2, 2):
+            paintBucket.backgroundColor = mixColors(first: "Blue", second: "Blue")
+        case (0, 1), (1, 0):
+            paintBucket.backgroundColor = mixColors(first: "Red", second: "Yellow")
+        case (0, 2), (2, 0):
+            paintBucket.backgroundColor = mixColors(first: "Red", second: "Blue")
+        case (1, 2), (2, 1):
+            paintBucket.backgroundColor = mixColors(first: "Yellow", second: "Blue")
+        default:
+            paintBucket.backgroundColor = UIColor.white
+        }
         
         
     }
