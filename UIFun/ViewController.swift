d3496116a8d3e0c216a95c64e47c445a664dc4d8
Maneesh <maneesh.yadav@ahold.com>
2017-04-17 19:31:14 +0200

    UI Lab: Painting By Numbers

--- UIFun/ViewController.swift
+++ UIFun/ViewController.swift
@@ -11,9 +11,17 @@ import UIKit
 class ViewController: UIViewController {
 
     // TODO: Setup the IBOutlets
+    @IBOutlet var colorImg: UIImageView!
+    @IBOutlet var firstColorSegmentedControl: UISegmentedControl!
+    @IBOutlet var secondColorSegmentedControl: UISegmentedControl!
+
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        colorSelected(firstColorSegmentedControl)
+        
     }
 
     func mixColors(first: String, second: String) -> UIColor {
@@ -40,7 +48,11 @@ class ViewController: UIViewController {
     @IBAction func colorSelected(_ sender: UISegmentedControl) {
         
         // TODO: Implement this function according to the instructions.
+        let str1 = firstColorSegmentedControl.titleForSegment(at: firstColorSegmentedControl.selectedSegmentIndex)
+        let str2 = secondColorSegmentedControl.titleForSegment(at: secondColorSegmentedControl.selectedSegmentIndex)
         
+        let color = mixColors(first: str1!, second: str2!)
+        colorImg.backgroundColor = color
         
     }
 
