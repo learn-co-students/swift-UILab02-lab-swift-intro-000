e87bfd4586a2b43da367b7c7ddafa1a01795535a
schrist <SimonChrist@gmx.de>
2017-04-19 19:41:53 +0200

    My Solution

--- UIFun/ViewController.swift
+++ UIFun/ViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 
     // TODO: Setup the IBOutlets
+    @IBOutlet weak var paintBucket: UIImageView!
+    @IBOutlet weak var firstColorSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var secondColorSegmentedControl: UISegmentedControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,8 +43,31 @@ class ViewController: UIViewController {
     @IBAction func colorSelected(_ sender: UISegmentedControl) {
         
         // TODO: Implement this function according to the instructions.
-        
-        
+        let firstSelection = firstColorSegmentedControl.selectedSegmentIndex
+        let secondSelection = secondColorSegmentedControl.selectedSegmentIndex
+        var firstColor: String
+        var secondColor: String
+        switch firstSelection {
+        case 0:
+            firstColor = "Red"
+        case 1:
+            firstColor = "Yellow"
+        case 2:
+            firstColor = "Blue"
+        default:
+            firstColor = "Red"
+        }
+        switch secondSelection {
+        case 0:
+            secondColor = "Red"
+        case 1:
+            secondColor = "Yellow"
+        case 2:
+            secondColor = "Blue"
+        default:
+            secondColor = "Red"
+        }
+        paintBucket.backgroundColor = mixColors(first: firstColor, second: secondColor)
     }
 
 }
