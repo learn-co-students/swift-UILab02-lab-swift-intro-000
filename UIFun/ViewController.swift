6eabf25760938a29fda59fd8c6d94454e92f4710
chelleccarlyle <chelle_carlyle@yahoo.com>
2017-07-08 16:01:45 -0700

    Implement segmented controls to mix colors and change color of the paint in the bucket

--- UIFun/ViewController.swift
+++ UIFun/ViewController.swift
@@ -11,9 +11,14 @@ import UIKit
 class ViewController: UIViewController {
 
     // TODO: Setup the IBOutlets
+    @IBOutlet weak var paintBucket: UIImageView!
+    @IBOutlet weak var firstColorSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var secondColorSegmentedControl: UISegmentedControl!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        paintBucket.backgroundColor = UIColor.red
     }
 
     func mixColors(first: String, second: String) -> UIColor {
@@ -40,7 +45,37 @@ class ViewController: UIViewController {
     @IBAction func colorSelected(_ sender: UISegmentedControl) {
         
         // TODO: Implement this function according to the instructions.
+        let firstSelection = firstColorSegmentedControl.selectedSegmentIndex
+        let secondSelection = secondColorSegmentedControl.selectedSegmentIndex
         
+        var firstColor: String
+        var secondColor: String
+        
+        //Get corresponding color from first color segmented control
+        switch firstSelection {
+            case 0:
+                firstColor = "Red"
+            case 1:
+                firstColor = "Blue"
+            case 2:
+                firstColor = "Yellow"
+            default:
+                firstColor = ""
+        }
+        
+        //Get corresponding color from second color segmented control
+        switch secondSelection {
+            case 0:
+                secondColor = "Red"
+            case 1:
+                secondColor = "Blue"
+            case 2:
+                secondColor = "Yellow"
+            default:
+                secondColor = ""
+        }
+        
+        paintBucket.backgroundColor = mixColors(first: firstColor, second: secondColor)
         
     }
 
