ab6014be3d330fd64ee3b33b12dda98f020cc28f
melbournenow <airliecamps@hotmail.com>
2017-08-12 11:27:27 +1000

    Complete problems

--- UIFun/ViewController.swift
+++ UIFun/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstColorSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var paintBucket: UIImageView!
+    
+    @IBOutlet weak var secondColorSegmentedControl: UISegmentedControl!
+    
     // TODO: Setup the IBOutlets
     
     override func viewDidLoad() {
@@ -38,7 +43,30 @@ class ViewController: UIViewController {
     }
     
     @IBAction func colorSelected(_ sender: UISegmentedControl) {
-        
+        let firstSelection = firstColorSegmentedControl.selectedSegmentIndex
+        let secondSelection = secondColorSegmentedControl.selectedSegmentIndex
+        switch (firstSelection, secondSelection) {
+        case (0,0):
+           paintBucket.backgroundColor = mixColors(first: "Red", second: "Red")
+        case(0,1):
+            paintBucket.backgroundColor = mixColors(first: "Red", second: "Yellow")
+        case (1,0):
+            paintBucket.backgroundColor = mixColors(first: "Yellow", second: "Red")
+        case (1,1):
+            paintBucket.backgroundColor = mixColors(first: "Yellow", second: "Yellow")
+        case (2,0):
+            paintBucket.backgroundColor = mixColors(first: "Blue", second: "Red")
+        case (2, 1):
+            paintBucket.backgroundColor = mixColors(first: "Blue", second: "Yellow")
+        case (0,2):
+            paintBucket.backgroundColor = mixColors(first: "Red", second: "Blue")
+        case (1,2):
+            paintBucket.backgroundColor = mixColors(first: "Yellow", second: "Blue")
+        case (2,2):
+            paintBucket.backgroundColor = mixColors(first: "Blue", second: "Blue")
+        default:
+            paintBucket.backgroundColor = mixColors(first: "Red", second: "Red")
+        }
         // TODO: Implement this function according to the instructions.
         
         
