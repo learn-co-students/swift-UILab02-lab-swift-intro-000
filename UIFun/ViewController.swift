4e6d1941aa3e5b2fde1b88b766f5881663236397
Alex <onischenko_ay@inbox.ru>
2017-12-01 19:50:52 +0300

    done

--- UIFun/ViewController.swift
+++ UIFun/ViewController.swift
@@ -11,9 +11,37 @@ import UIKit
 class ViewController: UIViewController {
 
     // TODO: Setup the IBOutlets
+    @IBOutlet weak var segment1: UISegmentedControl!
+    @IBOutlet weak var segment2: UISegmentedControl!
+    @IBOutlet weak var bucket: UIImageView!
+    
+    func getcolorfromint(pos: Int) -> String {
+        switch pos {
+        case 0:
+            return "Red"
+        case 1:
+            return "Yellow"
+        case 2:
+            return "Blue"
+        default: return ""
+        }
+    }
+    
+    func getColor(num: Int) -> String {
+        switch num {
+            case 1:
+            return getcolorfromint(pos: segment1.selectedSegmentIndex)
+            case 2:
+            return getcolorfromint(pos: segment2.selectedSegmentIndex)
+            default:
+            return ""
+            
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setColor()
     }
 
     func mixColors(first: String, second: String) -> UIColor {
@@ -36,12 +64,14 @@ class ViewController: UIViewController {
         }
         
     }
+
+    func setColor() {
+        let color = mixColors(first: getColor(num: 1), second: getColor(num: 2))
+        bucket.backgroundColor = color
+    }
     
     @IBAction func colorSelected(_ sender: UISegmentedControl) {
-        
-        // TODO: Implement this function according to the instructions.
-        
-        
+        setColor()
     }
 
 }
