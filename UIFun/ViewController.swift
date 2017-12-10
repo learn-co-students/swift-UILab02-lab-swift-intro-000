d06094c5ecfae7e2d84c28a49d019767398ac176
Sameh M AbuRadi <Sameh96m@icloud.com>
2017-12-10 23:25:19 +0200

    Add files via upload

---
+++ UIFun/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  UIFun
+//
+//  Created by Jim Campagno on 9/17/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // TODO: Setup the IBOutlets
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    func mixColors(first: String, second: String) -> UIColor {
+        
+        switch (first, second) {
+        case ("Red", "Red"):
+            return UIColor.red
+        case ("Red", "Yellow"), ("Yellow", "Red"):
+            return UIColor.orange
+        case ("Red", "Blue"), ("Blue", "Red"):
+            return UIColor.purple
+        case ("Yellow", "Yellow"):
+            return UIColor.yellow
+        case ("Yellow", "Blue"), ("Blue", "Yellow"):
+            return UIColor.green
+        case ("Blue", "Blue"):
+            return UIColor.blue
+        default:
+            return UIColor.white
+        }
+        
+    }
+    
+    @IBAction func colorSelected(_ sender: UISegmentedControl) {
+        
+        // TODO: Implement this function according to the instructions.
+        
+        
+    }
+
+}
