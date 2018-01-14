0bfb35a057ecd0e763b8ac677b2aea6edfedc030
Jaldhar H. Vyas <jaldhar@braincells.com>
2018-01-14 02:21:31 -0500

    Completed.

--- UIFun/ViewController.swift
+++ UIFun/ViewController.swift
@@ -9,39 +9,71 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    // TODO: Setup the IBOutlets
+    
+    @IBOutlet weak var paintBucket: UIImageView!
+    @IBOutlet weak var firstColorSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var secondColorSegmentedControl: UISegmentedControl!
+    var colorA: String = ""
+    var colorB: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        paintBucket.backgroundColor = mixColors(first: colorA, second: colorB)
     }
 
     func mixColors(first: String, second: String) -> UIColor {
         
         switch (first, second) {
-        case ("Red", "Red"):
+        case ("Red", "Red"), ("Red", ""), ("", "Red"):
             return UIColor.red
         case ("Red", "Yellow"), ("Yellow", "Red"):
             return UIColor.orange
         case ("Red", "Blue"), ("Blue", "Red"):
             return UIColor.purple
-        case ("Yellow", "Yellow"):
+        case ("Yellow", "Yellow"), ("Yellow", ""), ("", "Yellow"):
             return UIColor.yellow
         case ("Yellow", "Blue"), ("Blue", "Yellow"):
             return UIColor.green
-        case ("Blue", "Blue"):
+        case ("Blue", "Blue"), ("Blue", ""), ("", "Blue"):
             return UIColor.blue
         default:
             return UIColor.white
         }
-        
     }
     
     @IBAction func colorSelected(_ sender: UISegmentedControl) {
-        
-        // TODO: Implement this function according to the instructions.
-        
-        
-    }
 
+        if sender == firstColorSegmentedControl {
+            switch(firstColorSegmentedControl.selectedSegmentIndex) {
+            case 0:
+                colorA = "Red"
+                break
+            case 1:
+                colorA = "Yellow"
+                break
+            case 2:
+                colorA = "Blue"
+                break
+            default:
+                break
+            }
+        }
+        else if sender == secondColorSegmentedControl {
+            switch(secondColorSegmentedControl.selectedSegmentIndex) {
+            case 0:
+                colorB = "Red"
+                break
+            case 1:
+                colorB = "Yellow"
+                break
+            case 2:
+                colorB = "Blue"
+                break
+            default:
+                break
+            }
+        }
+        
+        paintBucket.backgroundColor = mixColors(first: colorA, second: colorB)
+    }
 }
