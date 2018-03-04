7c78d12d492be3fc4fb8baeac14185a4950ee28f
ericjlein <35666587+ericjlein@users.noreply.github.com>
2018-03-04 02:23:24 -0500

    Add files via upload

--- UIFun/ViewController.swift
+++ UIFun/ViewController.swift
@@ -9,38 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    // TODO: Setup the IBOutlets
+    
+    @IBOutlet weak var paintBucket: UIImageView!
+    @IBOutlet weak var firstColorSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var secondColorSegmentedControl: UISegmentedControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        paintBucket.backgroundColor = UIColor.white
     }
 
-    func mixColors(first: String, second: String) -> UIColor {
-        
-        switch (first, second) {
-        case ("Red", "Red"):
-            return UIColor.red
-        case ("Red", "Yellow"), ("Yellow", "Red"):
-            return UIColor.orange
-        case ("Red", "Blue"), ("Blue", "Red"):
-            return UIColor.purple
-        case ("Yellow", "Yellow"):
-            return UIColor.yellow
-        case ("Yellow", "Blue"), ("Blue", "Yellow"):
-            return UIColor.green
-        case ("Blue", "Blue"):
-            return UIColor.blue
-        default:
-            return UIColor.white
-        }
-        
-    }
+
     
     @IBAction func colorSelected(_ sender: UISegmentedControl) {
         
-        // TODO: Implement this function according to the instructions.
+        let firstSelection = firstColorSegmentedControl.selectedSegmentIndex
+        let secondSelection = secondColorSegmentedControl.selectedSegmentIndex
         
+        if firstSelection == 0 && secondSelection == 0 {
+            paintBucket.backgroundColor = UIColor.red
+        }
+        if firstSelection == 1 && secondSelection == 1 {
+            paintBucket.backgroundColor = UIColor.yellow
+        }
+        if firstSelection == 2 && secondSelection == 2 {
+            paintBucket.backgroundColor = UIColor.blue
+        }
+        else if (firstSelection == 0 && secondSelection == 1) || (firstSelection == 1 && secondSelection == 0) {
+            paintBucket.backgroundColor = UIColor.orange
+        }
+        else if (firstSelection == 0 && secondSelection == 2) || (firstSelection == 2 && secondSelection == 0) {
+            paintBucket.backgroundColor = UIColor.purple
+        }
+        else if (firstSelection == 1 && secondSelection == 2) || (firstSelection == 2 && secondSelection == 1) {
+            paintBucket.backgroundColor = UIColor.green
+        }
         
     }
 
