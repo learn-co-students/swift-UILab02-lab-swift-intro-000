911b148e9e737a66724b50601e1c1a100f34af4c
Jayymash <mashnijason@gmail.com>
2018-05-29 09:51:29 -0400

    Not sure if I did it wrong but it works

--- UIFun/ViewController.swift
+++ UIFun/ViewController.swift
@@ -12,36 +12,60 @@ class ViewController: UIViewController {
 
     // TODO: Setup the IBOutlets
     
+    @IBOutlet weak var paintBucket: UIImageView!
+    @IBOutlet weak var firstColorSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var secondColorSegmentedControl: UISegmentedControl!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        paintBucket.backgroundColor = UIColor.brown
     }
 
     func mixColors(first: String, second: String) -> UIColor {
         
         switch (first, second) {
         case ("Red", "Red"):
-            return UIColor.red
+            paintBucket.backgroundColor = UIColor.red
         case ("Red", "Yellow"), ("Yellow", "Red"):
-            return UIColor.orange
+           paintBucket.backgroundColor = UIColor.orange
         case ("Red", "Blue"), ("Blue", "Red"):
-            return UIColor.purple
+            paintBucket.backgroundColor = UIColor.purple
         case ("Yellow", "Yellow"):
-            return UIColor.yellow
+            paintBucket.backgroundColor = UIColor.yellow
         case ("Yellow", "Blue"), ("Blue", "Yellow"):
-            return UIColor.green
+            paintBucket.backgroundColor = UIColor.green
         case ("Blue", "Blue"):
-            return UIColor.blue
+            paintBucket.backgroundColor = UIColor.blue
         default:
             return UIColor.white
         }
-        
+       return UIColor.white
     }
     
     @IBAction func colorSelected(_ sender: UISegmentedControl) {
         
-        // TODO: Implement this function according to the instructions.
+        let firstSelection = firstColorSegmentedControl.selectedSegmentIndex
+        let secondSelection = secondColorSegmentedControl.selectedSegmentIndex
         
-        
+        switch (firstSelection, secondSelection){
+        case (0,0):
+            mixColors(first: "Red", second: "Red")
+            print ("Red, Red")
+        case (0,1), (1,0):
+            mixColors(first: "Red", second: "Yellow")
+        case (0,2), (2,0):
+            mixColors(first: "Red", second: "Blue")
+        case (1,1):
+            mixColors(first: "Yellow", second: "Yellow")
+        case (1,2), (2,1):
+            mixColors(first: "Yellow", second: "Blue")
+        case (2,2):
+            mixColors(first: "Blue", second: "Blue")
+            
+        default:
+            break
+        }
     }
 
 }
