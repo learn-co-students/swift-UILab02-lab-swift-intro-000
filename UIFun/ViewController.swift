a71d80bbc8a1e3f92ed130f054f9293b7df99b87
ezsmith <erin.smith.mail@gmail.com>
2018-06-10 18:38:51 -0700

    Paint bucket changing colour

--- UIFun/ViewController.swift
+++ UIFun/ViewController.swift
@@ -12,8 +12,14 @@ class ViewController: UIViewController {
 
     // TODO: Setup the IBOutlets
     
+    @IBOutlet weak var paintBucket: UIImageView!
+    @IBOutlet weak var firstColorSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var secondColorSegmentedControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        paintBucket.backgroundColor = UIColor.white
     }
 
     func mixColors(first: String, second: String) -> UIColor {
@@ -41,6 +47,42 @@ class ViewController: UIViewController {
         
         // TODO: Implement this function according to the instructions.
         
+        let firstSelection = firstColorSegmentedControl.selectedSegmentIndex
+        let secondSelection = secondColorSegmentedControl.selectedSegmentIndex
+        
+//        let firstSelection = selectedSegmentIndex
+        
+        switch (firstSelection, secondSelection) {
+        case (0, 0):
+            print(sender.selectedSegmentIndex)
+            paintBucket.backgroundColor = mixColors(first: "Red", second: "Red")
+        case (0, 1):
+            print(sender.selectedSegmentIndex)
+            paintBucket.backgroundColor = mixColors(first: "Red", second: "Yellow")
+        case (0, 2):
+            print(sender.selectedSegmentIndex)
+            paintBucket.backgroundColor = mixColors(first: "Red", second: "Blue")
+        case (1, 1):
+            print(sender.selectedSegmentIndex)
+            paintBucket.backgroundColor = mixColors(first: "Yellow", second: "Yellow")
+        case (1, 0):
+            print(sender.selectedSegmentIndex)
+            paintBucket.backgroundColor = mixColors(first: "Yellow", second: "Red")
+        case (1, 2):
+            print(sender.selectedSegmentIndex)
+            paintBucket.backgroundColor = mixColors(first: "Yellow", second: "Blue")
+        case (2, 2):
+            print(sender.selectedSegmentIndex)
+            paintBucket.backgroundColor = mixColors(first: "Blue", second: "Blue")
+        case (2, 0):
+            print(sender.selectedSegmentIndex)
+            paintBucket.backgroundColor = mixColors(first: "Blue", second: "Red")
+        case (2, 1):
+            print(sender.selectedSegmentIndex)
+            paintBucket.backgroundColor = mixColors(first: "Blue", second: "Yellow")
+        default:
+            paintBucket.backgroundColor = UIColor.white
+        }
         
     }
 
