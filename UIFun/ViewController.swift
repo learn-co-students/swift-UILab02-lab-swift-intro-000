388d9d7af2eb30cc73c04a956226162b52f3daa4
nalamoudi <nahoorii_91@hotmail.com>
2018-10-03 14:04:29 +0300

    Done paint job

--- UIFun/ViewController.swift
+++ UIFun/ViewController.swift
@@ -11,9 +11,15 @@ import UIKit
 class ViewController: UIViewController {
 
     // TODO: Setup the IBOutlets
+    @IBOutlet weak var paintBucket: UIImageView!
+    @IBOutlet weak var firstColorSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var secondColorSegmentedControl: UISegmentedControl!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        paintBucket.backgroundColor = UIColor.red
     }
 
     func mixColors(first: String, second: String) -> UIColor {
@@ -40,6 +46,29 @@ class ViewController: UIViewController {
     @IBAction func colorSelected(_ sender: UISegmentedControl) {
         
         // TODO: Implement this function according to the instructions.
+        var x1 = firstColorSegmentedControl.selectedSegmentIndex
+        
+        var x2 = secondColorSegmentedControl.selectedSegmentIndex
+        
+        if x1 == 0 && x2 == 0 {
+            paintBucket.backgroundColor = mixColors(first: "Red", second: "Red")
+        } else if x1 == 0 && x2 == 1{
+            paintBucket.backgroundColor = mixColors(first: "Red", second: "Yellow")
+        } else if x1 == 0 && x2 == 2{
+            paintBucket.backgroundColor = mixColors(first: "Red", second: "Blue")
+        } else if x1 == 1 && x2 == 0{
+            paintBucket.backgroundColor = mixColors(first: "Yellow", second: "Red")
+        } else if x1 == 1 && x2 == 1{
+            paintBucket.backgroundColor = mixColors(first: "Yellow", second: "Yellow")
+        } else if x1 == 1 && x2 == 2{
+            paintBucket.backgroundColor = mixColors(first: "Yellow", second: "Blue")
+        } else if x1 == 2 && x2 == 0{
+            paintBucket.backgroundColor = mixColors(first: "Blue", second: "Red")
+        } else if x1 == 2 && x2 == 1{
+            paintBucket.backgroundColor = mixColors(first: "Blue", second: "Yellow")
+        } else if x1 == 2 && x2 == 2{
+            paintBucket.backgroundColor = mixColors(first: "Blue", second: "Blue")
+        }
         
         
     }
