4a83f3c6ee600f3b9a835e0df1d44d77dfedd76b
Orvybr0wn1 <orvyb1@gmail.com>
2019-01-13 20:34:14 -0500

    Paint Bucket

--- UIFun/ViewController.swift
+++ UIFun/ViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 
     // TODO: Setup the IBOutlets
+    @IBOutlet weak var paintBucket: UIImageView!
+    @IBOutlet weak var firstColorSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var secondColorSegmentedControl: UISegmentedControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,8 +43,39 @@ class ViewController: UIViewController {
     @IBAction func colorSelected(_ sender: UISegmentedControl) {
         
         // TODO: Implement this function according to the instructions.
+        let firstColorSelected = firstColorSegmentedControl.selectedSegmentIndex
+        let secondColorSelected = secondColorSegmentedControl.selectedSegmentIndex
         
+        var color1: String = ""
+        var color2: String = ""
         
+        switch firstColorSelected {
+        case 0:
+            color1 = "Red"
+        case 1:
+            color1 = "Yellow"
+        case 2:
+            color1 = "Blue"
+            
+        default:
+            color1 = "Black"
+        }
+        
+        switch secondColorSelected {
+        case 0:
+            color2 = "Red"
+        case 1:
+            color2 = "Yellow"
+        case 2:
+            color2 = "Blue"
+            
+        default:
+            color2 = "Black"
+        }
+        
+        let paintColor = mixColors(first: color1, second: color2)
+        
+        paintBucket.backgroundColor = paintColor
     }
 
 }
